9dab1cc4b636cecf78774a91dab4ed2ebd3000c0
willow-12 <yosefayalew46@gmail.com>
2025-12-24 04:55:11 -0800

    Implement basic RMI calculator server

--- Server.java
+++ Server.java
@@ -1,1 +1,12 @@
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
+public class CalculatorServer {
+    public static void main(String[] args) throws Exception {
+        // optional: System.setProperty("java.rmi.server.hostname", "server-ip");
+        LocateRegistry.createRegistry(1099); // start rmiregistry in-process
+        CalculatorImpl calc = new CalculatorImpl();
+        Naming.rebind("CalculatorService", calc);
+        System.out.println("Calculator server ready");
+    }
+}
