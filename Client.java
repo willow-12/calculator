ec99e0823fa0df886cd229c8fdc2de6abedec08f
willow-12 <yosefayalew46@gmail.com>
2025-12-24 04:56:15 -0800

    Client for RMI operations

--- Client.java
+++ Client.java
@@ -1,1 +1,11 @@
+import java.rmi.Naming;
 
+public class CalculatorClient {
+    public static void main(String[] args) throws Exception {
+        String host = (args.length > 0) ? args[0] : "localhost";
+        Calculator calc = (Calculator) Naming.lookup("//" + host + "/CalculatorService");
+
+        System.out.println("2 + 3 = " + calc.add(2, 3));
+        System.out.println("10 / 2 = " + calc.divide(10, 2));
+    }
+}
